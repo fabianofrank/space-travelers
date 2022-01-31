fdc793169d145bc3162c2653994c493e655173b9
angelacuahutle <angela.cuahutle@gmail.com>
2022-01-31 11:50:35 -0600

    Remove unessesary files and provider and store imports as they are not in use now and cause linters issues

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  BrowserRouter as Router, Routes, Route,} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ContainerRockets from './containerRockets';
 import ContainerMissions from './containerMissions';
 import Navbar from '../components/navbar';
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import App from './containers/app';
-import store from './redux/configureStore';
 import './style.css';
 
 ReactDOM.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
