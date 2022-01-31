260b03f6468a014bb2e6e0d0c395e6f94bb1075f
fabianofrank <fabianofrank@protonmail.com>
2022-01-31 16:05:32 -0300

    Add Navlink

--- src/containers/app.js
+++ src/containers/app.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ContainerRockets from './containerRockets';
 import ContainerMissions from './containerMissions';
+import ContainerProfile from './containerProfile';
 import Navbar from '../components/navbar';
-import './style.css';
+import '../style.css';
 
 const App = () => (
   <Router>
@@ -11,6 +12,7 @@ const App = () => (
     <Routes>
       <Route exact path="/rockets" element={<ContainerRockets />} />
       <Route exact path="/missions" element={<ContainerMissions />} />
+      <Route exact path="/profile" element={<ContainerProfile />} />
     </Routes>
   </Router>
 );
