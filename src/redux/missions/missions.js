354ce2265ab1ab859d2b657106bb183e95e3b6fd
angelacuahutle <angela.cuahutle@gmail.com>
2022-02-01 16:52:20 -0600

    Fix linters

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,13 +1,11 @@
 import { fetchMissionsAPI } from '../apiHelper';
 
 const FETCH_MISSIONS = '.space-travelers/missions/FETCH_MISSIONS';
-// const JOIN_MISSION = 'space-travelers/missions/JOIN_MISSION';
 
 // const initialState = [];
 
 export const fetchMissions = () => async (dispatch) => {
   const missions = await fetchMissionsAPI();
-  console.log(missions);
   const missionsForDispatch = missions.map((mission) => ({
     mission_id: mission.mission_id,
     mission_name: mission.mission_name,
@@ -19,8 +17,6 @@ export const fetchMissions = () => async (dispatch) => {
     payload: missionsForDispatch,
   });
 };
-
-console.log(fetchMissions());
 
 // ADD REDUCER
 const missions = (state = [], action) => {
