75f521bc03f9c2cfe9e238a21df5796badd351d4
fabianofrank <fabianofrank@protonmail.com>
2022-02-02 16:45:09 -0300

    Add button and badges iteration

--- src/components/missions.js
+++ src/components/missions.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchMissions } from '../redux/missions/missions';
+import { fetchMissions, updateMissions } from '../redux/missions/missions';
 
 const Missions = () => {
   const missions = useSelector((state) => state.missions);
@@ -9,6 +9,22 @@ const Missions = () => {
   useEffect(() => {
     dispatch(fetchMissions());
   }, []);
+
+  const toggleReservation = (value = false, e) => {
+    const newState = missions.map((mission) => {
+      if (mission.id !== e.target.parentElement.parentElement.id) return mission;
+      return { ...mission, reserved: value };
+    });
+    return newState;
+  };
+
+  const joinMission = (e) => {
+    dispatch(updateMissions(toggleReservation(true, e)));
+  };
+
+  const leaveMission = (e) => {
+    dispatch(updateMissions(toggleReservation(false, e)));
+  };
 
   return (
     <div>
@@ -23,11 +39,17 @@ const Missions = () => {
         </thead>
         <tbody>
           {missions.map((missions) => (
-            <tr key={missions.id}>
+            <tr key={missions.id} id={missions.id}>
               <td>{missions.name}</td>
               <td>{missions.description}</td>
-              <td>reservation</td>
-              <td>{missions.reserved}</td>
+              {missions.reserved
+                ? (<td>ACTIVE MEMBER</td>)
+                : (<td>NOT A MEMBER</td>)}
+              <td>
+                {missions.reserved
+                  ? (<button onClick={leaveMission} type="button">Leave Mission</button>)
+                  : (<button onClick={joinMission} type="button">Join Mission</button>)}
+              </td>
             </tr>
           ))}
         </tbody>
--- src/containers/containerMissions.js
+++ src/containers/containerMissions.js
@@ -1,8 +1,4 @@
-// import React, { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { fetchMissionsAPI } from '../redux/apiHelper';
 import Missions from '../components/missions';
-// import './missions.css';
 
 function ContainerMissions() {
   return (
