08e9491f0f62db8ab1b5be8ed7878c536e922ce6
angelacuahutle <angela.cuahutle@gmail.com>
2022-02-02 13:39:07 -0600

    Create functions to reserve and cancel rreservation's rockets

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,10 +1,28 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchRockets } from '../redux/rockets/rockets';
+import { fetchRockets, updateRockets } from '../redux/rockets/rockets';
 
 const Rockets = () => {
   const rockets = useSelector((state) => state.rockets);
   const dispatch = useDispatch();
+
+  const rocketReservation = (value = false, e) => {
+    const returnReservedRocket = rockets.map((rocket) => {
+      if (rocket.id !== e.target.parentElement.parentElement.id) {
+        return rocket;
+      }
+      return { ...rocket, reserved: value };
+    });
+    return returnReservedRocket;
+  };
+
+  const handleReservation = (e) => {
+    dispatch(updateRockets(rocketReservation(true, e)));
+  };
+
+  const handleCancelation = (e) => {
+    dispatch(updateRockets(rocketReservation(false, e)));
+  };
 
   useEffect(() => {
     dispatch(fetchRockets());
@@ -14,13 +32,22 @@ const Rockets = () => {
     <div>
       <div>
         {rockets.map((rockets) => (
-          <div key={rockets.id}>
+          <div key={rockets.id} id={rockets.id}>
             <img src={rockets.image} alt="rocket" />
             <ul>
               <li>{rockets.name}</li>
+              <li>
+                {rockets.reserved ? (<span>Reserved</span>) : (<span className="none">display-none</span>)}
+                {rockets.description}
+              </li>
               <li>{rockets.description}</li>
               <li>{rockets.name}</li>
             </ul>
+            <div>
+              {rockets.reserved
+                ? (<button type="button" onClick={handleCancelation}>CANCEL RESERVATION</button>)
+                : (<button type="button" onClick={handleReservation}>RESERVE ROCKET</button>)}
+            </div>
           </div>
         ))}
       </div>
--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,6 +1,7 @@
 import { fetchRocketsAPI } from '../apiHelper';
 
 const FETCH_ROCKETS = '.space-travelers/rockets/FETCH_ROCKETS';
+const UPDATE_ROCKETS = '.space-travelers/rockets/UPDATE_ROCKETS';
 
 export const fetchRockets = () => async (dispatch) => {
   const rockets = await fetchRocketsAPI();
@@ -17,11 +18,18 @@ export const fetchRockets = () => async (dispatch) => {
   });
 };
 
+export const updateRockets = (payload) => ({
+  type: UPDATE_ROCKETS,
+  payload,
+});
+
 fetchRockets();
 
 const rockets = (state = [], action) => {
   switch (action.type) {
     case FETCH_ROCKETS:
+      return action.payload;
+    case UPDATE_ROCKETS:
       return action.payload;
     default:
       return state;
