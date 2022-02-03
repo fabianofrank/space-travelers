0acfe6ea7a27ba8bc47a621c8b24539d75da683b
angelacuahutle <angela.cuahutle@gmail.com>
2022-02-03 00:43:51 -0600

    Add style some parts are missing

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchRockets, updateRockets } from '../redux/rockets/rockets';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Rockets = () => {
   const rockets = useSelector((state) => state.rockets);
@@ -19,28 +20,30 @@ const Rockets = () => {
   }, []);
 
   return (
-    <div>
-      <div>
-        {rockets.map((rockets) => (
-          <div key={rockets.id} id={rockets.id}>
-            <img src={rockets.image} alt="rocket" />
-            <ul>
-              <li>{rockets.name}</li>
-              <li>
-                {rockets.reserved ? (<span>Reserved</span>) : (<span className="none">display-none</span>)}
-                {rockets.description}
-              </li>
-              <li>{rockets.description}</li>
-              <li>{rockets.name}</li>
-            </ul>
-            <div>
-              {rockets.reserved
-                ? (<button type="button" onClick={() => handleCancelation(rockets.id)}>CANCEL RESERVATION</button>)
-                : (<button type="button" onClick={() => handleReservation(rockets.id)}>RESERVE ROCKET</button>)}
+    <div className="container">
+      {rockets.map((rockets) => (
+        <div className="" key={rockets.id} id={rockets.id}>
+          <div className="d-flex border">
+            <div className="my-4 mx-4">
+              <img className="card" with="120" height="220" src={rockets.image} alt="rocket" />
+            </div>
+            <div className="w-50 text">
+              <ul className="rocket-list">
+                <li className="">
+                  {rockets.reserved ? (<span>Reserved</span>) : (<span className="d-print-none" />)}
+                </li>
+                <li className="card-title capitalized">{rockets.name}</li>
+                <li className="card-text">{rockets.description}</li>
+              </ul>
+              <div>
+                {rockets.reserved
+                  ? (<button type="button" className="btn btn-primary" onClick={() => handleCancelation(rockets.id)}>CANCEL RESERVATION</button>)
+                  : (<button type="button" className="btn btn-primary" onClick={() => handleReservation(rockets.id)}>RESERVE ROCKET</button>)}
+              </div>
             </div>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 };
